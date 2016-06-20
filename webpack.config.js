d861a47cab967ad88b2ff7afa4bd82333e260c3b
Luke Murphy <lukewm@riseup.net>
2016-06-20 14:13:26 +0200

    webpack config eslint fixups

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,22 @@
-var path              = require( 'path' );
-var webpack           = require( 'webpack' );
-var merge             = require( 'webpack-merge' );
-var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-var autoprefixer      = require( 'autoprefixer' );
-var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
-var CopyWebpackPlugin = require( 'copy-webpack-plugin' );
+var path              = require('path');
+var webpack           = require('webpack');
+var merge             = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var autoprefixer      = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
-console.log( 'WEBPACK GO!');
-
-// detemine build env
 var TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development';
 
-// common webpack config
 var commonConfig = {
-
   output: {
-    path:       path.resolve( __dirname, 'dist/' ),
+    path: path.resolve(__dirname, 'dist/'),
     filename: '[hash].js',
   },
-
   resolve: {
     modulesDirectories: ['node_modules'],
     extensions:         ['', '.js', '.elm']
   },
-
   module: {
     noParse: /\.elm$/,
     loaders: [
@@ -33,7 +26,6 @@ var commonConfig = {
       }
     ]
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',
@@ -41,27 +33,19 @@ var commonConfig = {
       filename: 'index.html'
     })
   ],
-
-  postcss: [ autoprefixer( { browsers: ['last 2 versions'] } ) ],
-
+  postcss: [autoprefixer({browsers: ['last 2 versions']})],
 }
 
-// additional webpack settings for local env (when invoked by 'npm start')
-if ( TARGET_ENV === 'development' ) {
-  console.log( 'Serving locally...');
-
-  module.exports = merge( commonConfig, {
-
+if (TARGET_ENV === 'development') {
+  module.exports = merge(commonConfig, {
     entry: [
       'webpack-dev-server/client?http://localhost:8080',
-      path.join( __dirname, 'src/index.js' )
+      path.join(__dirname, 'src/index.js')
     ],
-
     devServer: {
       inline:   true,
       progress: true
     },
-
     module: {
       loaders: [
         {
@@ -80,18 +64,12 @@ if ( TARGET_ENV === 'development' ) {
         }
       ]
     }
-
   });
 }
 
-// additional webpack settings for prod env (when invoked via 'npm run build')
-if ( TARGET_ENV === 'production' ) {
-  console.log( 'Building for prod...');
-
-  module.exports = merge( commonConfig, {
-
-    entry: path.join( __dirname, 'src/index.js' ),
-
+if (TARGET_ENV === 'production') {
+  module.exports = merge(commonConfig, {
+    entry: path.join(__dirname, 'src/index.js'),
     module: {
       loaders: [
         {
@@ -101,7 +79,7 @@ if ( TARGET_ENV === 'production' ) {
         },
         {
           test: /\.(css|scss)$/,
-          loader: ExtractTextPlugin.extract( 'style-loader', [
+          loader: ExtractTextPlugin.extract('style-loader', [
             'css-loader',
             'postcss-loader',
             'sass-loader'
@@ -109,7 +87,6 @@ if ( TARGET_ENV === 'production' ) {
         }
       ]
     },
-
     plugins: [
       new CopyWebpackPlugin([
         {
@@ -122,17 +99,12 @@ if ( TARGET_ENV === 'production' ) {
       ]),
 
       new webpack.optimize.OccurenceOrderPlugin(),
-
-      // extract CSS into a separate file
-      new ExtractTextPlugin( './[hash].css', { allChunks: true } ),
-
-      // minify & mangle JS/CSS
+      new ExtractTextPlugin('./[hash].css', {allChunks: true}),
       new webpack.optimize.UglifyJsPlugin({
-          minimize:   true,
-          compressor: { warnings: false }
-          // mangle:  true
+        minimize:   true,
+        compressor: {warnings: false},
+        mangle:  true,
       })
     ]
-
   });
 }
